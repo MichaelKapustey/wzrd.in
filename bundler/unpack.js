dd9e7862e1cc466db592045c174cf97cce962ea8
MichaelKapustey <michaelkapustey@gmail.com>
2016-09-13 13:52:11 +0300

    Update unpack.js
    I'm having issues with tar on windows. 
    
    1) It should be installed as separate app
    2) After installation I got an error 'cannot fork' because this function is not implemented in tar for windows
    3) I found hack  - use bsdtar and rename it to 'tar' but some new issues appeared
    
    There is great node tar that could avoid this overhead.

--- bundler/unpack.js
+++ bundler/unpack.js
@@ -1,16 +1,24 @@
-var gatherOutputs = require('./gather-outputs');
+var zlib = require('zlib')
+var = require('tar-fs');
 
 //
 // Unpack a tarball stream
 //
 module.exports = function unpack(env, stream, cb) {
-
-  //
-  // Ha! Ha! I'm using SYSTEM TAR!!
-  //
-  var tar = env.spawn('tar', [ '-xz' ]);
-
-  stream.pipe(tar.stdin);
-
-  gatherOutputs('tar', tar, cb);
+  stream
+        .pipe(zlib.createGunzip())
+        .on('error', cb)
+        .pipe(tar.extract(env.dirPath), {
+            map: function (header) {
+                header.name = header.name.replace(/^package\//, '');
+                return header;
+            }
+        })
+        .on('finish', function(code) {
+            if (code) {
+                return cb(new Error('tar exited with code '+ code));
+            }
+            cb();
+        })
+        .on('error', cb);
 };
